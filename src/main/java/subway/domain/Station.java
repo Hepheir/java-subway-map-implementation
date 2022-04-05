1362764c6bbd2705b7c04350565fa18e9cb5e1fb
Hepheir <hepheir@gmail.com>
2022-04-05 10:12:22 +0900

    refactor(domain/Station): assure `Station` created via constructor is valid
    생성자로 생성된 역은 올바른 노선임을 보장할 수 있는 형태로 리팩토링
    + 메소드명에서 중복되는 정보 제거

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -5,16 +5,16 @@ public class Station {
     private final String name;
 
     public Station(String name) throws IllegalArgumentException {
+        this.checkValid(name);
         this.name = name;
-        this.checkName();
     }
 
     public String getName() {
         return name;
     }
 
-    private void checkName() throws IllegalArgumentException {
-        if (this.name.length() < MINIMUM_NAME_LENGTH) {
+    private void checkValid(String name) throws IllegalArgumentException {
+        if (name.length() < MINIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException("지하철 역 이름은 2글자 이상이어야 합니다.");
         }
     }
