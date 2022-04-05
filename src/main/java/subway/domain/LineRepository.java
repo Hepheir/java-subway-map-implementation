a662f8879919e1657e37eb9ae926775f60e7055d
Hepheir <hepheir@gmail.com>
2022-04-05 11:15:26 +0900

    refactor(domain/LineRepository): remove redundancy in method names

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -12,15 +12,15 @@ public class LineRepository {
         return Collections.unmodifiableList(lines);
     }
 
-    protected static void addLine(Line line) {
+    protected static void add(Line line) {
         lines.add(line);
     }
 
-    protected static void deleteLine(Line line) {
+    protected static void delete(Line line) {
         lines.remove(line);
     }
 
-    protected static boolean hasLine(Line line) {
+    protected static boolean has(Line line) {
         return lines.stream().map(Line::getName).collect(Collectors.toList()).contains(line.getName());
     }
 }
