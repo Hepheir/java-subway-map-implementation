08f211ca340958f41448d58c3d502cc9e502d998
Hepheir <hepheir@gmail.com>
2022-04-06 02:54:38 +0900

    refactor(ApplicationInitializer): use `Model` for initializing

--- src/main/java/subway/ApplicationInitializer.java
+++ src/main/java/subway/ApplicationInitializer.java
@@ -1,34 +1,23 @@
 package subway;
 
-import subway.domain.Line;
-import subway.domain.LineRepository;
-import subway.domain.Station;
-import subway.domain.StationRepository;
+import subway.model.Model;
 
 public class ApplicationInitializer {
     public static void init() {
-        Station stationKD = new Station("교대역");
-        Station stationGN = new Station("강남역");
-        Station stationYS = new Station("역삼역");
-        Station stationNT = new Station("남부터미널역");
-        Station stationYJ = new Station("양재역");
-        Station stationYF = new Station("양재시민의숲역");
-        Station stationMB = new Station("매봉역");
+        initStations();
+    }
 
-        Line line2 = new Line("2호선", stationKD, stationGN, stationYS);
-        Line line3 = new Line("3호선", stationKD, stationNT, stationYJ, stationMB);
-        Line lineDX = new Line("신분당선", stationGN, stationYJ, stationYF);
+    private static void initStations() {
+        Model.createStation("교대역");
+        Model.createStation("강남역");
+        Model.createStation("역삼역");
+        Model.createStation("남부터미널역");
+        Model.createStation("양재역");
+        Model.createStation("양재시민의숲역");
+        Model.createStation("매봉역");
 
-        StationRepository.add(stationKD);
-        StationRepository.add(stationGN);
-        StationRepository.add(stationYS);
-        StationRepository.add(stationNT);
-        StationRepository.add(stationYJ);
-        StationRepository.add(stationYF);
-        StationRepository.add(stationMB);
-
-        LineRepository.add(line2);
-        LineRepository.add(line3);
-        LineRepository.add(lineDX);
+        Model.createLine("2호선", "교대역", "강남역", "역삼역");
+        Model.createLine("3호선", "교대역", "남부터미널역", "양재역", "매봉역");
+        Model.createLine("신분당선", "강남역", "양재역", "양재시민의숲역");
     }
 }
