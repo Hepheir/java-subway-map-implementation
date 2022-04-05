3e10c514925b0578fd5e8dc15322f72d91376d0e
Hepheir <hepheir@gmail.com>
2022-04-05 22:38:46 +0900

    refactor(view/View): remove dependencies except for `io.Output`
    `Output` 을 제외한 의존성을 최대한 제거해보았습니다.

--- src/main/java/subway/view/View.java
+++ src/main/java/subway/view/View.java
@@ -1,11 +1,7 @@
 package subway.view;
 
-import java.util.List;
+import java.util.Arrays;
 
-import subway.command.Command;
-import subway.controller.Controller;
-import subway.domain.Line;
-import subway.domain.Station;
 import subway.io.Output;
 
 public class View {
@@ -15,6 +11,10 @@ public class View {
 
     public static void printEmptyLine() {
         Output.printLine();
+    }
+
+    public static void printLine(String message) {
+        Output.printLine(message);
     }
 
     public static void printHeader(String message) {
@@ -29,43 +29,23 @@ public class View {
         Output.printLine(String.format("%s%s", PREFIX_ERROR, message));
     }
 
-    public static final void renderController(Controller controller) {
-        printHeader(controller.getLabel());
-        renderCommands(controller.commands());
-        printEmptyLine();
+    // Domain specifics
+
+    public static final void printScreenName(String name) {
+        printHeader(name);
     }
 
-    public static final void renderCommandGuide() {
+    public static final void printCommandFormat(Character key, String description) {
+        printLine(String.format("%c. %s", key, description));
+    }
+
+    public static final void printCommandPromptMessage() {
         printHeader("원하는 기능을 선택하세요.");
     }
 
-    public static final void renderSubwayMap() {
-        printHeader("지하철 노선도");
-        Line.getLines().forEach(line -> {
-            printInfo(line.getName());
-            printInfo("---");
-            line.stations().forEach(station -> printInfo(station.getName()));
-            printEmptyLine();
-        });
-    }
-
-    public static final void renderStationList() {
-        printHeader("역 목록");
-        Station.getStations().stream().map(Station::getName).forEach(View::printInfo);
-        printEmptyLine();
-    }
-
-    public static final void renderLineList() {
-        printHeader("노선 목록");
-        Line.getLines().stream().map(Line::getName).forEach(View::printInfo);
-        printEmptyLine();
-    }
-
-    private static final void renderCommands(List<Command> commands) {
-        commands.forEach(View::renderCommand);
-    }
-
-    private static final void renderCommand(Command command) {
-        Output.printLine(String.format("%c. %s", command.getKey(), command.getLabel()));
+    public static final void printLineOfSubwayMap(String lineName, String... stationNames) {
+        printInfo(lineName);
+        printInfo("---");
+        Arrays.asList(stationNames).forEach(View::printInfo);
     }
 }
