beee251ef3f01964a56123fb5c88d61801f7c719
Hepheir <hepheir@gmail.com>
2022-04-06 02:54:18 +0900

    refactor(enums/ScreenType): add `valuesExcept()`

--- src/main/java/subway/enums/ScreenType.java
+++ src/main/java/subway/enums/ScreenType.java
@@ -1,5 +1,8 @@
 package subway.enums;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum ScreenType {
     MAIN("메인 화면"),
     STATION_MANAGEMENT("구간 관리 화면"),
@@ -8,6 +11,13 @@ public enum ScreenType {
     SUBWAY_MAP("지하철 노선도");
 
     private final String value;
+
+    public static ScreenType[] valuesExcept(ScreenType... excludedScreenTypes) {
+        List<ScreenType> excludedScreenTypeList = Arrays.asList(excludedScreenTypes);
+        return Arrays.asList(ScreenType.values()).stream()
+            .filter(screenType -> !excludedScreenTypeList.contains(screenType))
+            .toArray(ScreenType[]::new);
+    }
 
     ScreenType(String value) {
         this.value = value;
