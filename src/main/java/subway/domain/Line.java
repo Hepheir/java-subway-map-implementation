4bbc7c34154427b4c01f76615b48902ac5478f76
Hepheir <hepheir@gmail.com>
2022-04-05 10:03:13 +0900

    refactor(domain/Line): refactor `delete(Station ?)` method
    - 내부의 (지워도 되는지 확인하는) 유효성 검사 로직을 별걔의 메소드로 분리
    - 제대로 삭제되었는지 확인하는 방법을 더 직관적으로 변경
        -> `List::remove`의 반환값 대신 `List::contains`로 사용

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -32,17 +32,22 @@ public class Line {
     }
 
     protected void delete(Station station) throws IllegalArgumentException {
-        if (this.stations.size() <= Line.MINIMUM_STATIONS_SIZE) {
-            throw new IllegalArgumentException("노선에 포함된 역이 두개 이하일 때는 역을 제거할 수 없습니다.");
-        }
-        if (!this.stations.remove(station)) {
-            throw new IllegalArgumentException("해당하는 역이 노선에 존재하지 않습니다.");
-        }
+        this.checkDeletable(station);
+        this.stations.remove(station);
     }
 
     private void checkValid(String name) throws IllegalArgumentException {
         if (name.length() < MINIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException("지하철 노선 이름은 2글자 이상이어야 합니다.");
+        }
+    }
+
+    private void checkDeletable(Station station) throws IllegalArgumentException {
+        if (this.stations.size() <= Line.MINIMUM_STATIONS_SIZE) {
+            throw new IllegalArgumentException("노선에 포함된 역이 두개 이하일 때는 역을 제거할 수 없습니다.");
+        }
+        if (!this.stations.contains(station)) {
+            throw new IllegalArgumentException("해당하는 역이 노선에 존재하지 않습니다.");
         }
     }
 }
