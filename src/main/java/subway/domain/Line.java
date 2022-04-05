f16f60f8accf4d7d7c25eddac2f5ad5f5934e3f5
Hepheir <hepheir@gmail.com>
2022-04-05 09:45:47 +0900

    refactor(domain/Line): remove dependacies of `Line`
    `Line`의 의존성 모두 제거
    - 더 이상 LineRepository에 의존하지 않음
    - 더 이상 Station(Repository)에 의존하지 않음

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -3,7 +3,6 @@ package subway.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 public class Line {
     private final static int MINIMUM_NAME_LENGTH = 2;
@@ -11,53 +10,17 @@ public class Line {
     private final String name;
     private final List<Station> stations = new ArrayList<>();
 
-    public static List<Line> getLines() {
-        return LineRepository.lines();
-    }
-
-    public static Line getLine(String name) throws IllegalArgumentException {
-        Optional<Line> foundLine = getLines().stream().filter(line -> line.getName().equals(name)).findFirst();
-        if (!foundLine.isPresent()) {
-            throw new IllegalArgumentException("해당하는 노선이 없습니다.");
-        }
-        return foundLine.get();
-    }
-
-    public static void registerLine(Line line) throws IllegalArgumentException {
-        if (line.stations().size() < MINIMUM_STATIONS_SIZE) {
-            throw new IllegalArgumentException("2개 이상의 역을 등록해야 합니다.");
-        }
-        LineRepository.addLine(line);
-    }
-
-    public static Line createLine(String name) throws IllegalArgumentException {
-        return new Line(name);
-    }
-
-    public static void deleteLine(String name) throws IllegalArgumentException {
-        Line.getLine(name).delete();
-    }
-
     public Line(String name) throws IllegalArgumentException {
         this.name = name;
         this.checkName();
-        this.checkDuplicate();
-    }
-
-    public String getName() {
-        return name;
     }
 
     public List<Station> stations() {
         return Collections.unmodifiableList(this.stations);
     }
 
-    public void addStation(String name) throws IllegalArgumentException {
-        Station.getStation(name).addLine(this);
-    }
-
-    public void register() throws IllegalArgumentException {
-        Line.registerLine(this);
+    public String getName() {
+        return name;
     }
 
     protected void addStation(Station station) {
@@ -81,15 +44,5 @@ public class Line {
         if (this.name.length() < MINIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException("지하철 노선 이름은 2글자 이상이어야 합니다.");
         }
-    }
-
-    private void checkDuplicate() throws IllegalArgumentException {
-        if (LineRepository.hasLine(this)) {
-            throw new IllegalArgumentException("이미 등록된 노선 이름입니다.");
-        }
-    }
-
-    private void delete() {
-        LineRepository.deleteLine(this);
     }
 }
