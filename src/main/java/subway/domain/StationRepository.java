01fb3a24f352a09b7ff53179a9a7229d5cb5a4a7
Hepheir <hepheir@gmail.com>
2022-04-05 11:18:43 +0900

    refactor(domain/StationRepository): make `lines()`, `add()`, `delete()` public

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -5,22 +5,53 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import subway.exception.OccupiedStationException;
+import subway.exception.StationAlreadyExsitsException;
+import subway.exception.StationNotFoundException;
+
 public class StationRepository {
     private static final List<Station> stations = new LinkedList<>();
 
-    protected static List<Station> stations() {
+    public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
 
-    protected static void add(Station station) {
+    public static void add(Station station) throws StationAlreadyExsitsException {
+        checkAddable(station);
         stations.add(station);
     }
 
-    protected static void delete(Station station) {
+    public static void delete(Station station) throws StationNotFoundException, OccupiedStationException {
+        checkDeletable(station);
         stations.remove(station);
     }
 
-    protected static boolean has(Station station) {
-        return stations.stream().map(Station::getName).collect(Collectors.toList()).contains(station.getName());
+    private static boolean contains(Station station) {
+        return stations.stream()
+            .map(Station::getName)
+            .collect(Collectors.toList())
+            .contains(station.getName());
+    }
+
+    private static void checkAddable(Station station) throws StationAlreadyExsitsException {
+        if (contains(station)) {
+            throw new StationAlreadyExsitsException();
+        }
+    }
+
+    private static void checkDeletable(Station station) throws StationNotFoundException, OccupiedStationException {
+        if (!contains(station)) {
+            throw new StationNotFoundException();
+        }
+        if (isOccupied(station)) {
+            throw new OccupiedStationException();
+        }
+    }
+
+    private static boolean isOccupied(Station station) {
+        return LineRepository.lines().stream()
+            .filter(line -> line.stations().contains(station))
+            .findAny()
+            .isPresent();
     }
 }
