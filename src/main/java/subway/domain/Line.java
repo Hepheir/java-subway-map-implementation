e211056e0d61fda5039cbc9f899452fabc7e3569
Hepheir <hepheir@gmail.com>
2022-04-05 10:33:01 +0900

    refactor(domain): use custom exceptions for `Line`, `Station`
    `exception` 패키지의 예외들을 사용하도록 변경하였습니다.

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -5,13 +5,17 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import subway.exception.InvalidStationNameException;
+import subway.exception.NotEnoughStationsException;
+import subway.exception.StationNotFoundException;
+
 public class Line {
     private final static int MINIMUM_NAME_LENGTH = 2;
     private final static int MINIMUM_STATIONS_SIZE = 2;
     private final String name;
     private final List<Station> stations = new ArrayList<>();
 
-    public Line(String name, Station... stations) throws IllegalArgumentException {
+    public Line(String name, Station... stations) throws InvalidStationNameException, NotEnoughStationsException {
         this.checkValid(name);
         this.checkEnough(stations);
         this.name = name;
@@ -34,29 +38,29 @@ public class Line {
         this.stations.add(index, station);
     }
 
-    public void delete(Station station) throws IllegalArgumentException {
+    public void delete(Station station) throws NotEnoughStationsException, StationNotFoundException {
         this.checkDeletable(station);
         this.stations.remove(station);
     }
 
-    private void checkValid(String name) throws IllegalArgumentException {
+    private void checkValid(String name) throws InvalidStationNameException {
         if (name.length() < MINIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException("지하철 노선 이름은 2글자 이상이어야 합니다.");
+            throw new InvalidStationNameException();
         }
     }
 
-    private void checkEnough(Station[] stations) throws IllegalArgumentException {
+    private void checkEnough(Station[] stations) throws NotEnoughStationsException {
         if (stations.length < MINIMUM_STATIONS_SIZE) {
-            throw new IllegalArgumentException("노선을 생성하기 위해 상행종점역과 하행종점역을 입력해야 합니다.");
+            throw new NotEnoughStationsException();
         }
     }
 
-    private void checkDeletable(Station station) throws IllegalArgumentException {
+    private void checkDeletable(Station station) throws NotEnoughStationsException, StationNotFoundException {
         if (this.stations.size() <= Line.MINIMUM_STATIONS_SIZE) {
-            throw new IllegalArgumentException("노선에 포함된 역이 두개 이하일 때는 역을 제거할 수 없습니다.");
+            throw new NotEnoughStationsException();
         }
         if (!this.stations.contains(station)) {
-            throw new IllegalArgumentException("해당하는 역이 노선에 존재하지 않습니다.");
+            throw new StationNotFoundException();
         }
     }
 }
--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,10 +1,12 @@
 package subway.domain;
 
+import subway.exception.InvalidStationNameException;
+
 public class Station {
     private final static int MINIMUM_NAME_LENGTH = 2;
     private final String name;
 
-    public Station(String name) throws IllegalArgumentException {
+    public Station(String name) throws InvalidStationNameException {
         this.checkValid(name);
         this.name = name;
     }
@@ -13,9 +15,9 @@ public class Station {
         return name;
     }
 
-    private void checkValid(String name) throws IllegalArgumentException {
+    private void checkValid(String name) throws InvalidStationNameException {
         if (name.length() < MINIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException("지하철 역 이름은 2글자 이상이어야 합니다.");
+            throw new InvalidStationNameException();
         }
     }
 }
