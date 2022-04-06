3aecf62a63ef1a4df06be8e96d7c0585df4bb2c9
Hepheir <hepheir@gmail.com>
2022-04-06 10:39:35 +0900

    refactor(io/Output): remove unnecessary string

--- src/main/java/subway/io/Output.java
+++ src/main/java/subway/io/Output.java
@@ -2,7 +2,7 @@ package subway.io;
 
 public class Output {
     public static void printLine() {
-        System.out.println("");
+        System.out.println();
     }
 
     public static void printLine(String s) {
